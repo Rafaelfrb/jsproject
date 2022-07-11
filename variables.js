f4355a00d87be82ba8588bf45db279bd867e35f8
Rafaelfrb <rafael.frb@hotmail.com>
2022-07-11 19:13:50 -0300

    Adding JS content

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name= "Rafael";
+
+console.log(name);
+
+//Variable names cannot be a reserved keyword
+//Should me meaningful
+//Cannot start with a number
+//Cannot contain space or hyphen
+//Are case-sensitive
