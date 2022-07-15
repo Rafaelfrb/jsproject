e37007ef8c1e8435b46a13a8aa4ce1831190c2e4
Rafaelfrb <rafael.frb@hotmail.com>
2022-07-15 18:59:59 -0300

    Adding JS content

--- primitivetypes.js
+++ primitivetypes.js
@@ -11,3 +11,10 @@ let isApproved = true; //Boolean Literal
 let firstName = undefined;
 let selectedCColor = null;
 
+//JavaScript is dynamically typed which means that a variable type can be changed during the 
+// execution of the program. If we use => typeof name - the result would be string
+// if we reasign the variable to 1 and use typeof name again it will result in number
+
+name = 1;
+
+
