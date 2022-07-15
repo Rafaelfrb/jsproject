69389d6eb5b15c68e6161c5be5672e9cf1d0fcf9
Rafaelfrb <rafael.frb@hotmail.com>
2022-07-14 23:01:40 -0300

    Adding JS content

---
+++ primitivetypes.js
@@ -0,0 +1,13 @@
+//Primitive types:
+// String
+// Numbers
+// Booleans
+// Undefined
+// Null
+
+let name = 'Rafael'; //String literal
+let age = 32; //Number Literal
+let isApproved = true; //Boolean Literal
+let firstName = undefined;
+let selectedCColor = null;
+
