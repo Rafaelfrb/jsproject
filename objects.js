1dd0817c9ad909def0641e429817518ecc1c3f0c
Rafaelfrb <rafael.frb@hotmail.com>
2022-07-16 22:27:51 -0300

    Adding JS content

---
+++ objects.js
@@ -0,0 +1,19 @@
+//Reference types in JavaScript:
+//Objects
+//Arrays
+//Functions
+
+//Example of Object:
+
+let person = {
+    name: 'Rafael',
+    age: 32
+};
+
+//Dot notation - one way of changing a property of an Object
+
+person.name = 'Nicolas';
+
+//Bracket notation - other way
+
+person['name'] = 'Rafael';
